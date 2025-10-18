3772ae316401c6b34f8b0c35d99719b470363181
Diego Sagredo <dsagredog@gmail.com>
2025-10-18 13:00:10 -0300

    update config deploy

--- src/services/getAllPortfolio.ts
+++ src/services/getAllPortfolio.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { ProjectT } from '../types/portfolioT';
 
 const API_BASE_ALL_PORTFOLIO = import.meta.env.VITE_API_BASE_DS;
 
-const getAllPortfolio = async () => {
+const getAllPortfolio = async (): Promise<ProjectT[]> => {
     try {
         const res = await axios.get(`${API_BASE_ALL_PORTFOLIO}/api/projects`);
         return res.data;
